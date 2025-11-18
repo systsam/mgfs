ac97262394e15488c5e12f49037ee0cdc4dc11ec
systsam <208572627+systsam@users.noreply.github.com>
2025-11-18 18:12:37 -0500

    progress

--- ag.py
+++ ag.py
@@ -1,53 +1,70 @@
 class Tensor:
 
-  def __init__(self, data, children, op='')
-    self.data = data.array # numpy array?
-    self.grad = 0 # initialized at 0, will be calculated and used other places
-    self.prev = set(children) # set object for multiple elements?
-    self.op = op
+  def __init__(self, data, children=(), op=''):
+      self.data = data
+      self.grad = 0.0 # initialized at 0, will be calculated and used other places
+      self.prev = set(children) # set eliminates duplicates 
+      self.op = op 
 
-  def __add__(self, other)
-    if other is not value then value(other) else other = other
-    out = Tensor(self.data + other.data, (self, other), '+')
+  def __add__(self, other):
+     # if other is not value then value(other) else other = other
+      out = Tensor(self.data + other.data, (self, other), '+')
 
-    return out
+      return out
 
-    def backward(self, other)
-      self.grad += out.grad * 1.0
-      other.grad += out.grad * 1.0
+      def backward(self, other):
+          self.grad += out.grad
+          other.grad += out.grad
+          
+      out.backward = backward
+      return out
+
 # chain rule for addition is passing on grad from child node
 # we want to increment to update grad in chain rule instance,
 # or updating the variables used in chain rule
 # we updated variables and so, probably want to return something?
 
 
-__mul__
-out = tensor(self.data * other.data, (self, other), '*')
+  def __mul__(self, other):
+      out = Tensor(self.data * other.data, (self, other), '*')
 
-       backward
-         self = other.grad * 1.0
-         other = self.grad * 1.0
+      return out
 
+      def backward(self, other):
+          self.grad += other.data * out.grad
+          other.grad += self.data * out.grad
 
-__tanh__
+      out.backward = backward
+      return out
 
-
-
-__relu__
-
-
-__neg__
+#__tanh__
 
 
 
-__sub__
+#__relu__
 
 
-__pow__
-
-
-
+#__neg__
 
 
 
+#__sub__
 
+
+#__pow__
+
+
+
+
+  def __repr__(self):
+    return f"Tensor(data={self.data}, grad={self.grad})"
+
+
+x = Tensor(2.0)
+y = Tensor(3.0)
+out = x*y
+out2 = x+out
+print(x)
+print(y)
+print(out)
+print(out2)
