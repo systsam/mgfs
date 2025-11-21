258d628188a93cac7e564d4728c2b370a85f19bf
systsam <208572627+systsam@users.noreply.github.com>
2025-11-21 12:00:12 -0500

    .

--- ag.py
+++ ag.py
@@ -5,36 +5,45 @@ class Tensor:
       self.grad = 0.0 # initialized at 0, will be calculated and used other places
       self.prev = set(children) # set eliminates duplicates 
       self.op = op 
+      self._backward = lambda: None
+
 
   def __add__(self, other):
-     # if other is not value then value(other) else other = other
+      other = other if isinstance(other, Tensor) else Tensor(other)
       out = Tensor(self.data + other.data, (self, other), '+')
 
-      return out
-
-      def backward(self, other):
+      def _backward():
           self.grad += out.grad
           other.grad += out.grad
           
-      out.backward = backward
+      out._backward = _backward
       return out
 
 # chain rule for addition is passing on grad from child node
 # we want to increment to update grad in chain rule instance,
 # or updating the variables used in chain rule
-# we updated variables and so, probably want to return something?
 
 
   def __mul__(self, other):
+      other = other if isinstance(other, Tensor) else Tensor(other)
       out = Tensor(self.data * other.data, (self, other), '*')
 
-      return out
-
-      def backward(self, other):
+      def _backward():
           self.grad += other.data * out.grad
           other.grad += self.data * out.grad
 
-      out.backward = backward
+      out._backward = _backward
+      return out
+
+  def __pow__(self, n):
+      # n = n if isinstance(n, Tensor) else Tensor(n)
+      out = Tensor(self.data ** n, (self,), '**{n}')
+
+      def _backward():
+          self.grad += other.data * out.grad
+          other.grad += self.data * out.grad
+
+      out._backward = _backward
       return out
 
 #__tanh__
@@ -51,20 +60,38 @@ class Tensor:
 #__sub__
 
 
-#__pow__
 
+  def backward(self):
+      topo = []
+      visited = set()
 
+      def build_topo(node):
+          if node not in visited:
+              visited.add(node)
+          for parent in node.prev:
+              build_topo(parent)
+          topo.append(node)
+   
+      build_topo(self)
 
+      self.grad = 1.0
+      for node in reversed(topo):
+          node._backward()
+      print(topo)
 
   def __repr__(self):
     return f"Tensor(data={self.data}, grad={self.grad})"
 
 
-x = Tensor(2.0)
-y = Tensor(3.0)
-out = x*y
-out2 = x+out
-print(x)
-print(y)
-print(out)
-print(out2)
+a = Tensor(2.0)
+b = Tensor(3.0)
+d = (a*b) + a
+d.backward()
+print("a value:", a.data,", b value:", b.data,", d value:", d.data)
+print("a grad:", a.grad,", b grad:", b.grad,", d grad:", d.grad)
+
+
+
+
+
+
